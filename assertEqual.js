1aec404c254b7e0e1527ff31decbe09543e76c99
Johannes Weber <johannes@drweber.de>
2021-10-20 17:23:56 -0700

    Add assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  //Console colors
+  let reset = "\x1b[0m";
+  let bgRed = "\x1b[41m";
+  let green = "\x1b[32m";
+  let passPrefix = green + "**PASS**" + reset + " ";
+  let failPrefix = bgRed + "**FAIL**" + reset + " ";
+
+  if (actual === expected) {
+    console.log(passPrefix + "Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log(failPrefix + "Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(1, -1);
