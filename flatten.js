0fce9a6f01261b149199c66b78244a1b30d61d4e
Johannes Weber <johannes@drweber.de>
2021-11-05 16:25:32 -0700

    Refactored flatten (mocha chai)

--- flatten.js
+++ flatten.js
@@ -1,27 +1,4 @@
-const assertArraysEqual = function(a, b) {
-  //Console colors
-  let reset = "\x1b[0m";
-  let bgRed = "\x1b[41m";
-  let green = "\x1b[32m";
-  let passPrefix = `${green} **PASS** ${reset}`;
-  let failPrefix = `${bgRed} **FAIL** ${reset}`;
-
-  if (eqArrays(a,b)) {
-    console.log(`${passPrefix} Assertion Passed: arrays are equal!`);
-  } else {
-    console.log(`${failPrefix} Assertion Failed: arrays are different!`);
-  }
-};
-
-const eqArrays = function(a,b) {
-  if (!(a && b && a.length === b.length))
-    return false;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i])
-      return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const flatten = function(a) {
   let output = [];
@@ -46,9 +23,4 @@ const flatten = function(a) {
   return output;
 };
 
-console.log("Single-layer tests:");
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]) ,[1, 2, 3, 4, 5, 6]);
-assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]) ,[1, 2, 3, 4, 5, 6]);
-console.log("Multi-layer tests:");
-assertArraysEqual(flatten([1, 2, [3, [4, 40, 41]], 5, [6]]) ,[1, 2, 3, 4, 40, 41, 5, 6]);
-assertArraysEqual(flatten([1, [2, [3, [4, [40, [41, [5, [6]]]]]]]]) ,[1, 2, 3, 4, 40, 41, 5, 6]);
+module.exports = flatten;
