ca245f61e738ac9b3a2f890bc29bf35d5e94d492
Johannes Weber <johannes@drweber.de>
2021-11-05 13:25:47 -0700

    Refactor assertEqual, head (module.exports)

--- assertEqual.js
+++ assertEqual.js
@@ -13,7 +13,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual(1, -1);
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  //Console colors
-  let reset = "\x1b[0m";
-  let bgRed = "\x1b[41m";
-  let green = "\x1b[32m";
-  let passPrefix = `${green} **PASS** ${reset}`;
-  let failPrefix = `${bgRed} **FAIL** ${reset}`;
+const assertEqual = require('./assertEqual');
 
-  if (actual === expected) {
-    console.log(`${passPrefix} Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`${failPrefix} Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
+//Return first element of array arg
 const head = function(arg) {
   if (arg.length > 0) {
     return arg[0];
@@ -20,7 +8,4 @@ const head = function(arg) {
   return undefined;
 };
 
-assertEqual(head([5,6,7]),5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["five"]), "five");
-assertEqual(head([]),undefined);
+module.exports = head;
