88b41abb8439bcd70bbec921b0180c7c265a1913
Johannes Weber <johannes@drweber.de>
2021-11-05 13:59:13 -0700

    Refactored tailTest.js (mocha chai)

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-console.log(assertEqual("Lighthouse,Labs", tail(["Hello", "Lighthouse", "Labs"]).join()));
-console.log(assertEqual("3,4", tail([2, 3, 4]).join()));
-console.log(assertEqual("3,March", tail([true, 3, "March"]).join()));
-console.log(assertEqual("", tail(["single"]).join()));
-console.log(assertEqual("", tail([]).join()));
-
-//Test case for original array (this doesn't really show it's unmodified though - just that its length stayed the same)
-const words = ["one", "two", "three"];
-tail(words);
-assertEqual(words.length, 3); //Only shows that the length of the array didn't change
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(tail([5]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
